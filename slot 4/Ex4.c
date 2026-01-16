caaedc7d124336106fcf8a532a61e9c20d878626
tandat1811 <tandat11206@gmail.com>
2026-01-16 11:41:47 +0700

    update

--- slot 4/Ex4.c
+++ slot 4/Ex4.c
@@ -2,12 +2,13 @@
 
 int main(){
 	int a,b;
-	char kytu;
+	char phepToan;
 	printf("Nhap 2 so a,b:");
 	scanf("%d,%d", &a,&b);
 	printf("Nhap phep toan:");
-	scanf(" %c", &kytu);
-	if (kytu == '/')
+	fflush(stdin);  // tránh bị lưu enter vào char hoặc xóa vùng đệm
+	scanf(" %c", &phepToan);
+	if (phepToan == '/')
 		if (b == 0)
 			printf ("khong the chia cho so 0");
  	 else 
