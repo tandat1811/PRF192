0def9914cf4a0b885072b0f57191cb3935710aa4
tandat1811 <tandat11206@gmail.com>
2026-01-21 09:11:44 +0700

    bai 4

---
+++ slot 5/bai 4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+	int money;
+	int sokwh;
+	printf("Nhap so kwh: ");
+	scanf ("%d", &sokwh);
+	if (sokwh<=100){
+		money = sokwh * 950;
+	} else if( sokwh <= 150 && sokwh > 100){
+		money = sokwh * 1250;
+	} else if( sokwh <= 200 && sokwh > 150){
+		money = sokwh * 1350;
+	} else{
+		money = sokwh * 1550;
+	}
+	printf("So tien can tra la: %d",money);
+	return 0;
+}
