07fa82564184055a45d52526190f8b27288dc078
tandat1811 <tandat11206@gmail.com>
2026-01-28 09:04:33 +0700

    update

--- Workshop/Workshop 1/Ex5.c
+++ Workshop/Workshop 1/Ex5.c
@@ -3,30 +3,32 @@
 int main(){
 	float soKm;
 	int thoiGian;
-	float Money;
+	float money;
+	float tong;
 	printf("Nhap so Km: ");
 	
-	if(scanf("%f", &soKm) != 1 || soKm<=0 ){
+	if(scanf("%f", &soKm) != 1 || soKm <=0 ){
 		printf("Khong tinh duoc");
 		return 0;
 	}
 	printf("Nhap thoi gian cho: ");
 	scanf("%d", &thoiGian);
-	float tong;
+	
 	
 	if(soKm <= 0.5){
-		Money = 12000;
+		money = 12000;
 	} else if (soKm > 0.5 && soKm <= 30){
-		Money = 12000 + ((soKm - 0.5) * 15000);
+		money = 12000 + ((soKm - 0.5) * 15000);
 	} else {
-		Money = 12000 + 29.5 * 12000 + ((soKm - 30) * 12000);
+		money = 12000 + 29.5 * 12000 + ((soKm - 30) * 12000);
 	}
-	tong = Money;
+	tong = money;
 	if(thoiGian > 5){
-		tong = (thoiGian - 5) * 1000 + Money;
+		tong = money + (thoiGian - 5) * 1000 ;
 	}
 	
-	printf("%-15s %-15s %-15s\n", "So Km", "Thoi gian cho", "Thanh tien");
-	printf("%-15.1f %-15d %-15.0f",soKm,thoiGian,tong);
+	printf("%-15s| %-15s| %-15s\n", "So Km", "Thoi gian cho", "Thanh tien");
+	printf("%-15.1f| %-15d| %-15.0f",soKm,thoiGian,tong);
 	return 0;
 }
+
--- Workshop/Workshop 1/Ex6.c
+++ Workshop/Workshop 1/Ex6.c
@@ -1,34 +1,26 @@
-#include <stdio.h>;
+#include <stdio.h>
 
 int main (){
 	float chuyenCan, giuaKy, cuoiKy;
 	float Tong;
 	char diemChu;
-	+
-	printf("Nhap diem chuyen can: ");
-	if(scanf("%f",&chuyenCan) != 1){
-	printf("Khong tinh duoc!!");
-	return 0;
-}
-	printf("Nhap diem giua ky: ");
-	if(scanf("%f",&giuaKy) != 1){
-	printf("Khong tinh duoc!!");
-	return 0;
-}
-	printf("Nhap diem cuoi ky: ");
-	if(scanf("%f",&cuoiKy) != 1){
-	printf("Khong tinh duoc!!");
-	return 0;
-}
-
-	Tong =((chuyenCan * 0.1) + (giuaKy * 0.3) + (cuoiKy * 0.5));
-	    if (Tong >= 8.5 && Tong <= 10)
+	
+	printf("Nhap diem chuyen can, giua ky, cuoi ky:");
+	int kq = scanf("%f %f %f",&chuyenCan, &giuaKy, &cuoiKy);
+	printf("kq nhap: %d",kq);
+	if(kq !=3 || chuyenCan <0 || giuaKy<0 || chuyenCan <0 ||cuoiKy<0 ||chuyenCan >10 ||giuaKy>10 ||cuoiKy>10)
+	{
+		printf("\nNhap sai diem, diem >=0!");
+		return 0;
+	}
+	Tong =((chuyenCan * 0.1) + (giuaKy * 0.3) + (cuoiKy * 0.6));
+	    if (Tong >= 8.5 )
         diemChu = 'A';
-    else if (Tong >= 7.0 && Tong < 8.5)
+    else if (Tong >= 7.0 )
         diemChu = 'B';
-    else if (Tong >= 5.5 && Tong < 7.0)
+    else if (Tong >= 5.5 )
         diemChu = 'C';
-    else if (Tong >= 4.0 && Tong < 5.5)
+    else if (Tong >= 4.0 )
         diemChu = 'D';
     else
         diemChu = 'F';
